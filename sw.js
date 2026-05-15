11c51d65fe58f10f1ee35e97b8e638918ed0088c
yoavkeinan-source <yoav.keinan@gmail.com>
2026-05-15 23:09:35 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'ginon-v10';
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
